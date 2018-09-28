f4d93e5fcd8276c802de69d9f2e27fb28c6caf85
robin <robin@robindeMac-mini.local>
2018-09-28 17:34:17 +0800

    Add new program

---
+++ src/js/bar.js
@@ -0,0 +1,4 @@
+export default function bar(){
+    console.log('this is bar!');
+    document.write("this is module bar!")
+}
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import bar from './bar';
+// import _ from 'lodash';
+bar();
+// function component(){
+//     let element = document.createElement('div');
+
+//     element.innerHTML = _.join(['Hello', 'webpack'],' ');
+
+//     return element;
+// }
+// document.appendChild(component());
